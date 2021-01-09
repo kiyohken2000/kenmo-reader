a5ffffab25e41f9bc1849ab8c36e4b3810bef120
kiyohken2000 <www.broadwell.com@gmail.com>
2021-01-10 01:59:50 +0900

    master

--- src/scenes/article/article.js
+++ src/scenes/article/article.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { Text, View, StatusBar, ScrollView } from 'react-native';
 import HTML from 'react-native-render-html';
 
 export default class Article extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {data: null};
+}
+
+componentDidMount() {
+	fetch(this.props.route.params.url)
+	.then((response) => response.text())
+	.then((responseText) => 
+		this.setState({ 
+			html: responseText,
+		})
+	)
+	.catch((error) => {
+		console.error(error);
+	});
+}
+
 	render() {
 		// const { params } = this.props.navigation.state;
 		const url = this.props.route.params.url
+		const htmlContent = this.state.html
 		return (
-			<View>
+			<ScrollView>
 				<StatusBar barStyle="light-content" />
-				<Text>{url}</Text>
-				<HTML
-          html={url}
-        />
-			</View>
+				<HTML html={ htmlContent } />
+			</ScrollView>
 		);
 	}
 }
