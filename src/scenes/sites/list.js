46597bea8595fb8268f17a09bef9798fc933f9c8
kiyohken2000 <www.broadwell.com@gmail.com>
2021-01-10 22:40:23 +0900

    master

---
+++ src/scenes/sites/list.js
@@ -0,0 +1,54 @@
+const sites = [
+  {
+    ID: 1,
+    name: '菊の門ニュース',
+    url: 'https://kikunomon.news'
+  },
+  {
+    ID: 2,
+    name: '保守速報',
+    url: 'https://hosyusokuhou.jp'
+  },
+  {
+    ID: 3,
+    name: 'VIDEOCARDZ',
+    url: 'https://videocardz.com'
+  },
+  {
+    ID: 4,
+    name: 'kitguru',
+    url: 'https://www.kitguru.net'
+  },
+  {
+    ID: 5,
+    name: 'wccftech',
+    url: 'https://wccftech.com'
+  },
+  {
+    ID: 6,
+    name: '菊の門ニュース2',
+    url: 'https://kikunomon.news'
+  },
+  {
+    ID: 7,
+    name: '保守速報2',
+    url: 'https://hosyusokuhou.jp'
+  },
+  {
+    ID: 8,
+    name: 'VIDEOCARDZ2',
+    url: 'https://videocardz.com'
+  },
+  {
+    ID: 9,
+    name: 'kitguru2',
+    url: 'https://www.kitguru.net'
+  },
+  {
+    ID: 10,
+    name: 'wccftech2',
+    url: 'https://wccftech.com'
+  },
+]
+
+export default sites;
--- src/scenes/sites/sites.js
+++ src/scenes/sites/sites.js
@@ -1,52 +1,170 @@
 import React from 'react'
-import { Text, View, StatusBar, ScrollView, TouchableOpacity } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Text, View, ScrollView, TouchableOpacity, StyleSheet, Modal } from 'react-native'
+import { Card, ListItem, Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/Feather'
+import Storage from 'react-native-storage'
+import AsyncStorage from '@react-native-community/async-storage'
+
+import sites from './list'
+
+const storage = new Storage({
+  storageBackend: AsyncStorage,
+  defaultExpires: null,
+  enableCache: false,
+});
+global.storage = storage;
 
 export default class Sites extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      isVisible: false,
+      sites: [],
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadStrage();
+  }
+
+  loadStrage() {
+    global.storage.getAllDataForKey('site')
+      .then(res => {
+        this.setState({data: res})
+      });
+    }
+
+  _sites = () => {
+  };
+
+  showModal() {
+    this.setState({isVisible: true});
+  }
+ 
+  closeModal() {
+    this.setState({isVisible: false});
+  }
+
 	render() {
-    const sites = [
-      {
-        name: '菊の門ニュース',
-        url: 'https://kikunomon.news'
-      },
-      {
-        name: '保守速報',
-        url: 'https://hosyusokuhou.jp'
-      },
-      {
-        name: 'VIDEOCARDZ',
-        url: 'https://videocardz.com'
-      },
-      {
-        name: 'kitguru',
-        url: 'https://www.kitguru.net'
-      },
-      {
-        name: 'wccftech',
-        url: 'https://wccftech.com'
-      },
-    ]
-
-
+    var viewSites = this.state.data
 		return (
-			<ScrollView>
-				<StatusBar barStyle="light-content" />
-          {
-            sites.map((u, i) => {
-              return (
+      <View style={styles.container}>
+        <View style={styles.content}>
+			    <ScrollView>
+            {
+              viewSites.map((u, i) => {
+                return (
                   <Card key={i}>
                     <TouchableOpacity
                       onPress={() => this.props.navigation.navigate('NewsList', { url: u.url })}
                     >
-                      <Text>{u.name}</Text>
-                      <Text>{u.url}</Text>
+                      <View style={{ flexDirection: 'row'}}>
+                        <View>
+                          <Text>{u.siteid}</Text>
+                          <Text>{u.name}</Text>
+                          <Text>{u.url}</Text>
+                        </View>
+                        <View style={{ position: 'absolute', right: 0 }}>
+                          <TouchableOpacity
+                            onPress={() => {
+                              global.storage.remove({
+                                key: 'site',
+                                id: u.siteid,
+                              });
+                              this.loadStrage()
+                            }}
+                          >
+                            <Icon name="x" size={20} color="green"/>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
                     </TouchableOpacity>
                   </Card>
-              );
-            })
-          }
-			</ScrollView>
+                );
+              })
+            }
+          </ScrollView>
+        </View>
+        <View style={styles.Overlay}>
+          <TouchableOpacity onPress={() => this.showModal()}>
+            <Icon name="plus-circle" size={65} color="orange"/>
+          </TouchableOpacity>
+        </View>
+
+        <Modal
+          visible={this.state.isVisible}
+          transparent={false}
+          animationType={"slide" || "fade"}
+          presentationStyle={"fullScreen" || "pageSheet" || "formSheet" || "overFullScreen"}
+        >
+          <View style={styles.container}>
+            <Card>
+              <Text>利用可能なサイトの一覧です</Text>
+            </Card>
+            <ScrollView>
+              {
+                sites.map((u, i) => {
+                 return (
+                  <Card key={i}>
+                    <View style={{ flexDirection: 'row'}}>
+                      <View>
+                        <Text>{u.ID}</Text>
+                        <Text>{u.name}</Text>
+                        <Text>{u.url}</Text>
+                      </View>
+                      <View style={{ position: 'absolute', right: 0 }}>
+                        <TouchableOpacity
+                          onPress={() => {
+                            var siteData = {
+                              siteid: u.ID,
+                              name: u.name,
+                              url: u.url,
+                            }
+                            global.storage.save({
+                              key: 'site',
+                              id: u.ID,
+                              data: siteData,
+                            });
+                            this.loadStrage()
+                          }}
+                        >
+                          <Icon name="plus-circle" size={40} color="orange"/>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </Card>
+                  );
+                })
+              }
+            </ScrollView>
+          </View>
+          <View style={styles.Overlay}>
+            <TouchableOpacity onPress={() => this.closeModal()}> 
+              <Icon name="x-circle" size={65} color="blue"/>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+
+      </View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  Overlay: {
+    flex: 1,
+    position: "absolute",
+    opacity: 1.0,
+    bottom: 25,
+    right: 30,
+    justifyContent: "center",
+  },
+});
