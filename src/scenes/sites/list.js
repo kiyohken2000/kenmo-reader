23017f27dc0e59e5b42b80b554c84e0590a8e5f4
kiyohken2000 <www.broadwell.com@gmail.com>
2021-01-18 03:51:39 +0900

    master

--- src/scenes/sites/list.js
+++ src/scenes/sites/list.js
@@ -2,14 +2,74 @@ export const lastUpdate = '2021-01-18AM'
 
 export const sites = [
   {
-    ID: 75,
-    name: '芸能スクープ、今旬ナビ！！',
-    url: 'http://imashun-navi.com'
+    ID: 6,
+    name: 'ロケットニュース24',
+    url: 'https://rocketnews24.com'
   },
   {
-    ID: 74,
-    name: '日刊SPA！',
-    url: 'https://nikkan-spa.jp'
+    ID: 9,
+    name: '秒刊SUNDAY',
+    url: 'https://yukawanet.com'
+  },
+  {
+    ID: 94,
+    name: '東スポＷeb',
+    url: 'https://www.tokyo-sports.co.jp'
+  },
+  {
+    ID: 93,
+    name: 'MEN’S NON-NO WEB',
+    url: 'https://www.mensnonno.jp'
+  },
+  {
+    ID: 91,
+    name: 'BRIDGE',
+    url: 'https://thebridge.jp'
+  },
+  {
+    ID: 90,
+    name: 'トレタメ',
+    url: 'https://toretame.jp'
+  },
+  {
+    ID: 89,
+    name: 'にじめん',
+    url: 'https://nijimen.net'
+  },
+  {
+    ID: 87,
+    name: 'エンタメOVO',
+    url: 'https://tvfan.kyodo.co.jp'
+  },
+  {
+    ID: 86,
+    name: 'アゴラ',
+    url: 'http://agora-web.jp'
+  },
+  {
+    ID: 85,
+    name: 'ネトセツ',
+    url: 'https://net-torisetsu.jp'
+  },
+  {
+    ID: 81,
+    name: 'grape',
+    url: 'https://grapee.jp'
+  },
+  {
+    ID: 84,
+    name: 'ダ・ヴィンチニュース',
+    url: 'https://ddnavi.com'
+  },
+  {
+    ID: 77,
+    name: 'オトナライフ',
+    url: 'https://otona-life.com'
+  },
+  {
+    ID: 76,
+    name: 'おたくま経済新聞',
+    url: 'https://otakei.otakuma.net'
   },
   {
     ID: 73,
@@ -17,29 +77,9 @@ export const sites = [
     url: 'https://smart-flash.jp'
   },
   {
-    ID: 72,
-    name: 'アサ芸プラス',
-    url: 'https://www.asagei.com'
-  },
-  {
     ID: 71,
     name: '気になる芸能ニュースまとめ',
     url: 'https://unicorntomo.com'
-  },
-  {
-    ID: 70,
-    name: '芸トピ',
-    url: 'https://geitopi.com'
-  },
-  {
-    ID: 69,
-    name: '日刊サイゾー',
-    url: 'https://www.cyzo.com'
-  },
-  {
-    ID: 68,
-    name: 'cyzo woman',
-    url: 'https://www.cyzowoman.com'
   },
   {
     ID: 67,
@@ -52,19 +92,9 @@ export const sites = [
     url: 'https://oggi.jp'
   },
   {
-    ID: 65,
-    name: 'WWD JAPAN',
-    url: 'https://www.wwdjapan.com'
-  },
-  {
     ID: 64,
     name: 'グルメプレス',
     url: 'https://gourmetpress.net'
-  },
-  {
-    ID: 63,
-    name: 'DIAMOND Chain Store',
-    url: 'https://diamond-rm.net'
   },
   {
     ID: 62,
@@ -76,90 +106,25 @@ export const sites = [
     name: 'GetNavi',
     url: 'https://getnavi.jp'
   },
-  /*{
-    ID: 60,
-    name: 'ひみつのどうくつ',
-    url: 'https://himitsu-ch.com'
-  },*/
-  {
-    ID: 59,
-    name: 'イイ・アクセス',
-    url: 'https://iiaccess.net'
-  },
   {
     ID: 58,
     name: 'かーずSP',
     url: 'http://www.karzusp.net'
   },
   {
-    ID: 1,
-    name: '菊ノ紋ニュース',
-    url: 'https://kikunomon.news'
-  },
-  {
-    ID: 2,
-    name: '保守速報',
-    url: 'https://hosyusokuhou.jp'
-  },
-  {
-    ID: 3,
-    name: 'VIDEOCARDZ',
-    url: 'https://videocardz.com'
-  },
-  {
-    ID: 4,
-    name: 'kitguru',
-    url: 'https://www.kitguru.net'
-  },
-  {
-    ID: 5,
-    name: 'wccftech',
-    url: 'https://wccftech.com'
-  },
-  {
-    ID: 6,
-    name: 'ロケットニュース24',
-    url: 'https://rocketnews24.com'
-  },
-  {
     ID: 7,
     name: 'watch@2チャンネル',
     url: 'http://www.watch2chan.com'
-  },
-  /*{
-    ID: 8,
-    name: 'やらおん！*',
-    url: 'http://yaraon-blog.com'
-  },*/
-  {
-    ID: 9,
-    name: '秒刊SUNDAY',
-    url: 'https://yukawanet.com'
-  },
-  {
-    ID: 10,
-    name: 'BUZZmag (ばずまぐ)',
-    url: 'https://buzzmag.jp'
   },
   {
     ID: 11,
     name: 'NETGEEK',
     url: 'http://netgeek.biz'
   },
-  /*{
-    ID: 12,
-    name: '9ポスト*',
-    url: 'https://9post.tv'
-  },*/
   {
     ID: 13,
     name: 'Temita-てみた',
     url: 'https://temita.jp'
-  },
-  {
-    ID: 14,
-    name: 'BUSHOO!JAPAN',
-    url: 'https://bushoojapan.com'
   },
   {
     ID: 15,
@@ -182,19 +147,9 @@ export const sites = [
     url: 'https://gori.me'
   },
   {
-    ID: 19,
-    name: 'リブウェル！',
-    url: 'https://livewell-m.com'
-  },
-  {
     ID: 20,
     name: 'iPhone Mania',
     url: 'https://iphone-mania.jp'
-  },
-  {
-    ID: 21,
-    name: 'iTea4.0',
-    url: 'http://itea40.jp'
   },
   {
     ID: 22,
@@ -207,59 +162,14 @@ export const sites = [
     url: 'https://link-man.net'
   },
   {
-    ID: 24,
-    name: 'IPHONEな日々',
-    url: 'https://iphonenahibi.com'
-  },
-  {
-    ID: 25,
-    name: 'iPhoneまとめブログ',
-    url: 'https://mobile-shuri.jp/matome'
-  },
-  {
     ID: 25,
     name: 'チメブロ',
     url: 'https://chimeblo.com'
   },
   {
-    ID: 26,
-    name: '愛ちゃんのブログ',
-    url: 'https://aithan-japanese.com'
-  },
-  {
-    ID: 27,
-    name: '70年代80年代Fanブログ',
-    url: 'https://70-80fan.com'
-  },
-  {
     ID: 28,
     name: 'ハコログ',
     url: 'https://hakolog.net'
-  },
-  {
-    ID: 29,
-    name: 'パチンコ屋の倒産を応援するブログ',
-    url: 'https://pachitou.com'
-  },
-  {
-    ID: 30,
-    name: '小坪しんやのHP〜行橋市議会議員',
-    url: 'https://samurai20.jp'
-  },
-  {
-    ID: 31,
-    name: '遠藤健太郎オフィシャルブログ',
-    url: 'http://endokentaro.shinhoshu.com'
-  },
-  {
-    ID: 32,
-    name: '【厳選】洒落怖まとめ',
-    url: 'https://horror2ch.com'
-  },
-  {
-    ID: 33,
-    name: '洒落怖.com',
-    url: 'https://xn--f8tx20an6r.com'
   },
   {
     ID: 34,
@@ -271,11 +181,6 @@ export const sites = [
     name: 'トキノドロップ',
     url: 'https://tokinodrop.tokyo'
   },
-  /*{
-    ID: 36,
-    name: 'アノニマス ポスト',
-    url: 'https://anonymous-post.mobi'
-  },*/
   {
     ID: 36,
     name: 'Tech速：てくそく',
@@ -307,11 +212,6 @@ export const sites = [
     url: 'https://chomanga.org'
   },
   {
-    ID: 42,
-    name: 'マンガちゃんねる',
-    url: 'https://manga-ch.com'
-  },
-  {
     ID: 43,
     name: 'ジャンプまとめ速報',
     url: 'https://jumpmatome2ch.biz'
@@ -321,11 +221,6 @@ export const sites = [
     name: 'コミック速報',
     url: 'https://comisoku.com'
   },
-  /*{
-    ID: 45,
-    name: 'あにまんch',
-    url: 'https://animanch.com'
-  },*/
   {
     ID: 46,
     name: 'かみすくアニメ',
@@ -346,11 +241,6 @@ export const sites = [
     name: 'マクリン',
     url: 'https://makuring.com'
   },
-  /*{
-    ID: 50,
-    name: 'CAMPFIRE',
-    url: 'https://bamp.media'
-  },*/
   {
     ID: 51,
     name: 'ROOMIE',
@@ -367,11 +257,6 @@ export const sites = [
     url: 'https://jisin.jp'
   },
   {
-    ID: 54,
-    name: '共同通信',
-    url: 'https://www.kyodo.co.jp'
-  },
-  {
     ID: 55,
     name: '流通ニュース',
     url: 'https://www.ryutsuu.biz'
@@ -380,6 +265,226 @@ export const sites = [
     ID: 56,
     name: 'LNEWS',
     url: 'https://www.lnews.jp'
+  },
+  {
+    ID: 97,
+    name: 'usedoor',
+    url: 'https://usedoor.jp'
+  },
+  {
+    ID: 96,
+    name: 'サッカーキング',
+    url: 'https://www.soccer-king.jp'
+  },
+  {
+    ID: 95,
+    name: 'BASEBALL KING',
+    url: 'https://baseballking.jp'
+  },
+  /*{
+    ID: 92,
+    name: 'ザ・ファッションポスト',
+    url: 'https://fashionpost.jp'
+  },*/
+  {
+    ID: 88,
+    name: 'おたぽる',
+    url: 'https://otapol.com'
+  },
+  /*{
+    ID: 83,
+    name: '9ポスト',
+    url: 'https://9post.tv'
+  },*/
+  {
+    ID: 80,
+    name: 'TOCANA',
+    url: 'https://tocana.jp'
+  },
+  {
+    ID: 79,
+    name: 'wezzy',
+    url: 'https://wezz-y.com'
+  },
+  {
+    ID: 78,
+    name: 'オモコロ',
+    url: 'https://omocoro.jp'
+  },
+  {
+    ID: 75,
+    name: '芸能スクープ、今旬ナビ！！',
+    url: 'http://imashun-navi.com'
+  },
+  {
+    ID: 74,
+    name: '日刊SPA！',
+    url: 'https://nikkan-spa.jp'
+  },
+  {
+    ID: 72,
+    name: 'アサ芸プラス',
+    url: 'https://www.asagei.com'
+  },
+  {
+    ID: 70,
+    name: '芸トピ',
+    url: 'https://geitopi.com'
+  },
+  {
+    ID: 69,
+    name: '日刊サイゾー',
+    url: 'https://www.cyzo.com'
+  },
+  {
+    ID: 68,
+    name: 'cyzo woman',
+    url: 'https://www.cyzowoman.com'
+  },
+  {
+    ID: 65,
+    name: 'WWD JAPAN',
+    url: 'https://www.wwdjapan.com'
+  },
+  {
+    ID: 63,
+    name: 'DIAMOND Chain Store',
+    url: 'https://diamond-rm.net'
+  },
+  /*{
+    ID: 60,
+    name: 'ひみつのどうくつ',
+    url: 'https://himitsu-ch.com'
+  },*/
+  {
+    ID: 59,
+    name: 'イイ・アクセス',
+    url: 'https://iiaccess.net'
+  },
+  {
+    ID: 1,
+    name: '菊ノ紋ニュース',
+    url: 'https://kikunomon.news'
+  },
+  {
+    ID: 2,
+    name: '保守速報',
+    url: 'https://hosyusokuhou.jp'
+  },
+  {
+    ID: 3,
+    name: 'VIDEOCARDZ',
+    url: 'https://videocardz.com'
+  },
+  {
+    ID: 4,
+    name: 'kitguru',
+    url: 'https://www.kitguru.net'
+  },
+  {
+    ID: 5,
+    name: 'wccftech',
+    url: 'https://wccftech.com'
+  },
+  /*{
+    ID: 8,
+    name: 'やらおん！*',
+    url: 'http://yaraon-blog.com'
+  },*/
+  {
+    ID: 10,
+    name: 'BUZZmag (ばずまぐ)',
+    url: 'https://buzzmag.jp'
+  },
+  /*{
+    ID: 12,
+    name: '9ポスト*',
+    url: 'https://9post.tv'
+  },*/
+  {
+    ID: 14,
+    name: 'BUSHOO!JAPAN',
+    url: 'https://bushoojapan.com'
+  },
+  {
+    ID: 19,
+    name: 'リブウェル！',
+    url: 'https://livewell-m.com'
+  },
+  {
+    ID: 21,
+    name: 'iTea4.0',
+    url: 'http://itea40.jp'
+  },
+  {
+    ID: 24,
+    name: 'IPHONEな日々',
+    url: 'https://iphonenahibi.com'
+  },
+  {
+    ID: 25,
+    name: 'iPhoneまとめブログ',
+    url: 'https://mobile-shuri.jp/matome'
+  },
+  {
+    ID: 26,
+    name: '愛ちゃんのブログ',
+    url: 'https://aithan-japanese.com'
+  },
+  {
+    ID: 27,
+    name: '70年代80年代Fanブログ',
+    url: 'https://70-80fan.com'
+  },
+  {
+    ID: 29,
+    name: 'パチンコ屋の倒産を応援するブログ',
+    url: 'https://pachitou.com'
+  },
+  {
+    ID: 30,
+    name: '小坪しんやのHP〜行橋市議会議員',
+    url: 'https://samurai20.jp'
+  },
+  {
+    ID: 31,
+    name: '遠藤健太郎オフィシャルブログ',
+    url: 'http://endokentaro.shinhoshu.com'
+  },
+  {
+    ID: 32,
+    name: '【厳選】洒落怖まとめ',
+    url: 'https://horror2ch.com'
+  },
+  {
+    ID: 33,
+    name: '洒落怖.com',
+    url: 'https://xn--f8tx20an6r.com'
+  },
+  /*{
+    ID: 36,
+    name: 'アノニマス ポスト',
+    url: 'https://anonymous-post.mobi'
+  },*/
+  {
+    ID: 42,
+    name: 'マンガちゃんねる',
+    url: 'https://manga-ch.com'
+  },
+  /*{
+    ID: 45,
+    name: 'あにまんch',
+    url: 'https://animanch.com'
+  },*/
+  /*{
+    ID: 50,
+    name: 'CAMPFIRE',
+    url: 'https://bamp.media'
+  },*/
+  {
+    ID: 54,
+    name: '共同通信',
+    url: 'https://www.kyodo.co.jp'
   },
   {
     ID: 57,
